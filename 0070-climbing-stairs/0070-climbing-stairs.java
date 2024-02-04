403304e57edd66d5d3cde5b0be877b9826f407f0
Bhuvan Gour <bhuvangour990@gmail.com>
2024-02-04 23:23:21 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (29.12%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,20 @@
+class Solution {
+ public static int climbStairs(int n) {
+
+        if (n <= 2) {
+            return n;
+        }
+
+        int x = 1, y = 2;
+        for (int i = 2; i < n; i++) {
+            int steps = x;
+            x = y;
+            y = steps + x;
+        }
+        return y;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(climbStairs(5));
+    }
+}
