81089e685b6a823e9f23dbc3e722e625f2bcdeb4
Bhuvan Gour <bhuvangour990@gmail.com>
2024-02-05 23:24:23 +0530

    Time: 1 ms (74.09%), Space: 41.3 MB (8.90%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,21 @@
+class Solution {
+public boolean isHappy(int n) {
+        
+        int sum = 0;
+
+        while (n != 1 && n != 4) {
+            
+            char[] digits = String.valueOf(n).toCharArray();
+            sum = 0;
+            
+            for (char digitChar : digits) {
+                int digit = digitChar - '0';
+                sum += digit * digit;
+            }
+            
+            n = sum;
+        }
+
+        return n == 1;
+    }
+}
