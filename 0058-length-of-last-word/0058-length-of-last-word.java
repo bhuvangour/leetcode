324feb2091e9753a6cfc7e53f063782142b7a3bc
Bhuvan Gour <bhuvangour990@gmail.com>
2024-02-20 23:03:50 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (69.71%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+            s = s.trim();
+        int lastWordIndex = s.lastIndexOf(' ') + 1;
+
+        return ( s.length() - lastWordIndex );  
+
+    }
+}
